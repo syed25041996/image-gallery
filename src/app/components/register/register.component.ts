deaddf7b8737cebb7e5def438c3a5f053e94b66d
syed25041996 <syed25041996@gmail.com>
2023-04-05 23:38:41 +0530

    first commit

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  saveRegisterDetails(data:any){
+    console.log(data.value)
+  }
+}
